af4a7e368cb581c5997d019f57b2dc56485bca25
Cole Bermudez <cole@systemsmd.local>
2021-11-17 12:02:02 -0700

    Sync priv repo with pub

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,6 +1,15 @@
 Start-Transcript -Append C:\Support\Logs\PostDeploymentCleanupLog.txt
 
-Start-Sleep -Seconds 120
+function Start-Sleep($seconds) {
+    $doneDT = (Get-Date).AddSeconds($seconds)
+    while($doneDT -gt (Get-Date)) {
+        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
+        $percent = ($seconds - $secondsLeft) / $seconds * 100
+        Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining $secondsLeft -PercentComplete $percent
+        [System.Threading.Thread]::Sleep(300000)
+    }
+    Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining 0 -Completed
+}
 
 # Reset Privacy settings to default
 reg delete HKLM\SOFTWARE\Policies\Microsoft\Windows\OOBE /v DisablePrivacyExperience /f
@@ -12,18 +21,12 @@ REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdmi
 REG DELETE "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
 
 # Start Automate installer in quiet mode
-msiexec /i C:\Support\Installers\Automate-test.msi /q /L*V "C:\Support\Logs\automate.log"
-
-$filePath = 'C:\Windows\LTSvc'
-while (-not (Test-Path -Path $filePath)) {
-    ## Wait a specific interval
-    Start-Sleep -Seconds 300
-}
+Start-Process msiexec -Wait -ArgumentList '/I C:\Support\Installers\Automate-test.msi /quiet'
 
 # Run WindowsSetup2_0-WIP
 # Forked from Cole's GitHub repo
 
-PowerShell -ExecutionPolicy Bypass -File C:\Support\Scripts\WindowsSetup2_0-WIP.ps1
+PowerShell -ExecutionPolicy Bypass -File "C:\Support\Scripts\WindowsSetup2_0.ps1"
 
 Remove-Item -Path C:\\Support\\Scripts -Recurse -Verbose
 Remove-Item -Path C:\\Support\\Installers -Recurse -Verbose
