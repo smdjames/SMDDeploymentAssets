83ddae5db2163ad142873423c732894c605908ef
Cole Bermudez <cole@systemsmd.local>
2021-11-18 09:13:51 -0700

    Reverting

--- cleanup.ps1
+++ cleanup.ps1
@@ -26,9 +26,9 @@ Start-Process msiexec -Wait -ArgumentList '/I C:\Support\Installers\Automate-tes
 # Run WindowsSetup2_0-WIP
 # Forked from Cole's GitHub repo
 
-#PowerShell -ExecutionPolicy Bypass -File "C:\Support\Scripts\WindowsSetup2_0.ps1"
+Start-Process PowerShell.exe '& C:\Support\Scripts\WindowsSetup2_0.ps1'
 
-#Remove-Item -Path C:\\Support\\Scripts -Recurse -Verbose
-#Remove-Item -Path C:\\Support\\Installers -Recurse -Verbose
+Remove-Item -Path C:\\Support\\Scripts -Recurse -Verbose
+Remove-Item -Path C:\\Support\\Installers -Recurse -Verbose
 
 Stop-Transcript
