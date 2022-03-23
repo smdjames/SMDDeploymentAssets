9c332d224dd49a447375f507dbf630231a0242bf
Cole Bermudez <cole@systemsmd.local>
2022-03-23 10:40:26 -0600

    Updated chocolatey URL

--- WindowsSetup2_0-WIP.ps1
+++ WindowsSetup2_0-WIP.ps1
@@ -120,7 +120,7 @@ REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f /v TargetRelea
 
 Write-Host -ForegroundColor Green "Install Chocolatey to automate basic program installation"
 #install Chocolatey and other programs
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
 
 #Install Java
